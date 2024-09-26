73c4052570bae2ed290845e6fcc692e634e085c2
kshitejkhankal <kshitejkhankal@gmsil.com>
2024-09-26 12:26:00 +0530

    modified user data

--- install.sh
+++ install.sh
@@ -2,9 +2,12 @@
 sudo apt-get update
 sudo snap install aws-cli --classic -y
 sudo snap install terraform --classic -y
+sudo snap install git --classic -y
+sudo snap install jenkins --classic -y
 sudo apt update
 sudo apt install fontconfig openjdk-17-jre
 java -version
 openjdk version "17.0.8" 2023-07-18
 OpenJDK Runtime Environment (build 17.0.8+7-Debian-1deb12u1)
 OpenJDK 64-Bit Server VM (build 17.0.8+7-Debian-1deb12u1, mixed mode, sharing)
+
