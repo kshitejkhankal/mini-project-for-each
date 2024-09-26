2f516135dfa2a236457bab7cdaffcdc4c7eeea84
kshitejkhankal <kshitejkhankal@gmsil.com>
2024-09-26 18:08:27 +0530

    modify

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!bin/sh
 sudo apt-get update
-sudo snap install aws-cli --classic -y
-sudo snap install terraform --classic -y
-sudo snap install git --classic -y
-sudo snap install jenkins --classic -y
+sudo snap install aws-cli --classic 
+sudo snap install terraform --classic 
 sudo apt update
-sudo apt install fontconfig openjdk-17-jre
+sudo apt install fontconfig openjdk-17-jre -y
 java -version
 openjdk version "17.0.8" 2023-07-18
 OpenJDK Runtime Environment (build 17.0.8+7-Debian-1deb12u1)
 OpenJDK 64-Bit Server VM (build 17.0.8+7-Debian-1deb12u1, mixed mode, sharing)
+sudo snap install jenkins --classic 
+
 
